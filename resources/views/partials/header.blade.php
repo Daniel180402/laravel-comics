8f41334b3dbeee388ee01bd5e60d0261c700b4b6
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-04 17:09:52 +0200

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,62 @@
 <header>
-    <h1>This is a header</h1>
+    <div class="my-container">
+        <div class="header-logo">
+            <img src="images/dc-logo.png" alt="DC Comics main logo">
+        </div>
+
+        <nav class="header-nav">
+            <ul>
+                <li>
+                    <a :href="">
+                        CHARACTERS
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        COMICS
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        MOVIES
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        GAMES
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        COLLECTIBLES
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        VIDEOS
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        FANS
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        NEWS
+                    </a>
+                </li>
+                <li>
+                    <a :href="">
+                        SHOP
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
